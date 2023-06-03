35e9e9157fdf25eef00a6cd47e608c4bd9aa8c8c
silver0108 <kk1170@naver.com>
2023-06-04 02:02:50 +0900

    [Merge] 병합 충돌 확인 및 수정

--- pybo/views/vote_views.py
+++ pybo/views/vote_views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
 
-from ..models import Question, Answer
+from ..models import Question, Answer, Comment
 
 
 @login_required(login_url='common:login')
